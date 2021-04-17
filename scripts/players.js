29dda3429dc2e0089ee7558575cc47e4f5c9b32c
Billy Joe R. Santos <billyjoersantos@outlook.com>
2021-04-17 01:35:18 -0300

    Initial commit

---
+++ scripts/players.js
@@ -0,0 +1,75 @@
+let latestEdit = {};
+
+const posts = [];
+
+const api = "https://jsonplaceholder.typicode.com/posts";
+
+function loadPosts() {
+    const mainEl = document.querySelector("#posts");
+
+    mainEl.innerHTML = '';
+
+    const req = new XMLHttpRequest();
+
+    console.log(posts);
+
+    if (posts.length > 0) {
+        for (let index = 0; index < posts.length; index++) {
+            const element = posts[index];
+            
+            req.open("GET", `${api}/${index + 1}`);            
+            
+            req.send(null);
+        }
+
+        req.addEventListener("loadend", () => {
+            for (let index = 0; index < posts.length; index++) {
+                const element = posts[index];
+
+                const liEl = document.createElement("li");
+                    
+                liEl.innerHTML = element.player;
+
+                mainEl.appendChild(liEl);
+            }
+
+        });
+
+    } else {
+        alert("Adicione algo primeiro para poder ver!");
+    }
+    
+}
+
+function postData($event) {
+    const form = document.querySelector("#playersform");
+
+    $event.preventDefault();
+
+    const data = { player: form.elements.player.value, description: form.elements.description.value, id: posts.length + 1, time: new Date() };
+
+    const req = new XMLHttpRequest();
+
+    req.open("POST", api);
+
+    req.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    
+    req.send(JSON.stringify(data));
+
+    req.addEventListener("load", () => {        
+        const response = JSON.parse(req.response);
+
+        posts.push(response);
+
+        latestEdit = response;        
+
+        localStorage.setItem('latest', JSON.stringify(latestEdit));
+
+        form.reset();
+
+        loadPosts();
+
+        console.log("Post request sent!")
+    });
+
+}
---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const lastPost = JSON.parse(localStorage.getItem('latest')) || [];
+
+loadLastPost();
+
+function loadLastPost() {
+    const container = document.getElementById("container");
+
+    const lastPostTitleContainer = container.firstElementChild;
+
+    const lastPostContentContainer = document.querySelector("article");
+
+    if (lastPost.lenght != 0) {
+        lastPostTitleContainer.innerHTML = `Último post atualizado: ${lastPost.player}`;
+
+        const lastPostTime = lastPost.time;
+
+        const lastPostTitle = lastPostTitleContainer.innerHTML;
+
+        lastPostTitleContainer.innerHTML = `${lastPostTitle} - ${lastPostTime}`;
+
+        lastPostContentContainer.innerHTML = `<p>${lastPost.description}</p>`;
+    } else {
+        lastPostTitleContainer.innerHTML = "Crie uma postagem para um jogador para que ela apareça aqui";
+
+        lastPostContentContainer.innerHTML = `<p>Aqui vai toda a história do jogador, escrita livremente por você, em formato wiki</p>`;
+    }
+    
+}
